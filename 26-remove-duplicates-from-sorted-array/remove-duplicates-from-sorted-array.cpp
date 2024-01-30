ecff9092bfc06bc3382c721cf1683b87ce714b1a
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-01-30 18:11:29 +0530

    Time: 3 ms (97.55%) | Memory: 21.1 MB (11.41%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int prev = 0;
+        int prev = 0; // will point to the place where there mustr be unique num and before it everyone is unique
         int index =1;        
         while(index<nums.size()){
-            if(nums[index] == nums[prev]){
+            if(nums[index] == nums[prev]){ // check if there is duplicate or not
                 index++;
             }
-            else{
+            else{  // replace the prev place to a unique num
                 prev++;
                 nums[prev] = nums[index];
                 index++;
