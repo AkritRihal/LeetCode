d757e298bf58571a0aa96d878b0d9cfc378367d7
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-01-31 02:20:31 +0530

    Time: 90 ms (41.21%) | Memory: 62.1 MB (12.06%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+       int n  = arr.size();
+       int start  = 0;
+       int end = n-1;
+       int mid = start +(end - start)/2;
+       while (start<end){
+           if(arr[mid]<arr[mid+1]){
+               // a wali line mein hu 
+               //right mein jana padega
+               start = mid+1;  // +1 because peak cant be in left side because of the condition
+           }
+           else{
+               // peak point pr hu ya b wali line pr hu
+               // a wali line jana padega, left side
+            //    equal to mid , so that we do not miss the mid 
+               end = mid;
+           }
+           mid = start + (end-start)/2;
+       } 
+       return start;
+    }
+};
