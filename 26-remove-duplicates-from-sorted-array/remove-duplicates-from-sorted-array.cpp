2b4833f69b42e04e7cf7d8734aa407a57af0474e
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-01-30 18:09:36 +0530

    Time: 7 ms (78.42%) | Memory: 21 MB (11.41%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int prev = 1;
-        // both the pointers will start from the index 1, 
-        // here prev points to the place where unique values must come.
-        //this is kind of fast slow pointer
-        //every time a unique element is found it is assigned to the prev index and prev in icremented 
-        
-        for(int index = 1;index<nums.size(); index++){
-            if(nums[index] != nums[index-1]){
-                nums[prev] = nums[index];
+        int prev = 0;
+        int index =1;        
+        while(index<nums.size()){
+            if(nums[index] == nums[prev]){
+                index++;
+            }
+            else{
                 prev++;
+                nums[prev] = nums[index];
+                index++;
             }
         }
-
-
-        return prev;
+        return prev+1;
     }
 };
