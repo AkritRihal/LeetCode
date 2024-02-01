f3bb74bde3ae2720233afac4c54ded12ffc64be1
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-02-01 17:00:46 +0530

    Time: 3 ms (67.33%) | Memory: 13.5 MB (8.00%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -75,3 +75,54 @@ public:
         return -1;
     }
 };
+
+
+
+//solution with pivot index
+//  class Solution {
+// public:
+//     int FindPivotIndex(vector<int>& nums) {
+//         int left = 0, right = nums.size() - 1;
+        
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+            
+//             if (nums[mid] > nums[right]) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+        
+//         return left;
+//     }
+    
+//     int binarySearch(vector<int>& nums, int target, int left, int right) {
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+            
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] < target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//         }
+        
+//         return -1;
+//     }
+    
+//     int search(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         if (n == 0) return -1;
+        
+//         int pivotIndex = FindPivotIndex(nums);
+        
+//         if (pivotIndex == 0 || target < nums[0]) {
+//             return binarySearch(nums, target, pivotIndex, n - 1);
+//         } else {
+//             return binarySearch(nums, target, 0, pivotIndex - 1);
+//         }
+//     }
+// };
