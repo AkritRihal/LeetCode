cb3b6c0fd13c2b7f903725016ee849134c3ccf1c
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-02-05 15:21:56 +0530

    Time: 3 ms (53.24%) | Memory: 7.4 MB (24.74%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     static string str;
+    //custom comparator
     static bool compare(char ch1, char ch2){
+        // / this will return true, if postion of ch1 in str sting is less than the positin of  ch2 in str string
+        //when true is returned then ch1 wioll be placed before ch2 in output string 
         return (str.find(ch1)<str.find(ch2));
     }
 
     string customSortString(string order, string s) {
         str= order; //to use this in the function created
-        sort(s.begin(),s.end(),compare);
+        sort(s.begin(),s.end(),compare); //sorting is dependent on the compare func,  
         return s;
     }
 };
