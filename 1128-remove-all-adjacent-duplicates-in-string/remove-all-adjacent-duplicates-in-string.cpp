560f581322b1160c0d3c3444bea787206918b462
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-02-06 00:40:14 +0530

    Time: 53 ms (25.24%) | Memory: 11 MB (88.60%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         int i =0;
+
+        //you did it onyour own
+        // check next if same then erase, then point index to previous element
         while(i<s.length()){
             if(s[i]==s[i+1]){
                 s.erase(i,2);
