797320b6b6ae53cb6f3a3c352b2b63326738e3f1
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-02-08 18:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (27.89%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int nums =0,sign=1,i=0;
+        // sign 1 means positive value
+        while(s[i]==' ') i++;// checking whitespaces
+
+        if(i<s.length() && (s[i]=='-' || s[i]=='+')) {
+            sign = s[i]=='+'?1:-1;
+            i++;
+        }// checking sign
+        while(i<s.length() && isdigit(s[i])){
+            if(nums>INT_MAX/10 || (nums==INT_MAX/10 && s[i]>'7')){
+                return sign ==-1?INT_MIN:INT_MAX;
+            } 
+            nums = nums*10+(s[i]-'0'); //s[i]-'0' will result in integer ex: let s[i] = 1; so ascii of 1 = 49 and ascii 0 = 28 so 49-28 =1
+            i++;
+        }
+        
+        return nums*sign;        
+    }
+};
