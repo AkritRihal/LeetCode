f19d8cdc23d9a6c9b6bc04f77f9e30e4c2a24639
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-02-23 19:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (62.25%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
 
-    string sumStr(string num1, int p1, string num2, int p2, int carry){
+    void sumStr(string& num1, int p1, string& num2, int p2, int carry, string& ans){
 
         //base case
         if(p1 < 0 && p2 < 0){
             if(carry != 0){
-                return string(1, carry + '0');
+                ans.push_back(carry + '0');
             }
-            return "";
+            return;
         }
 
         //add
-        string answer = "";
+        
         //converting string to int and then adding and then calling recurssion func
         int n1 = ((p1 >= 0)? num1[p1]:'0') - '0';
         int n2 = ((p2 >= 0)? num2[p2]:'0') - '0';
@@ -20,19 +20,17 @@ public:
         int sum = n1 +n2 + carry;
          
         int digit = sum % 10;
-        answer.push_back(digit + '0'); // int to str
+        ans.push_back(digit + '0'); // int to str
         carry = sum / 10;
 
         //recursion  
-
-        answer += sumStr(num1, p1-1, num2, p2-1, carry);
-
-        return answer;
+        sumStr(num1, p1-1, num2, p2-1, carry, ans);
 
     }
 
     string addStrings(string num1, string num2) {
-        string ans  = sumStr(num1, num1.size()-1, num2, num2.size()-1, 0);
+        string ans = "";
+        sumStr(num1, num1.size()-1, num2, num2.size()-1,0 ,  ans);
         reverse(ans.begin(),ans.end());
         return ans;
         
